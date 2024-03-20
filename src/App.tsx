0f32d39c9b3150a758debeb9997994dd5222d970
zhekaasd <zhekaasd13@gmail.com>
2024-03-20 04:17:41 +0400

    hw last week on sprint 1

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
       {/* <HW1 /> */}
-      <HW2 />
-      {/*<HW3 />*/}
-      {/*<HW4 />*/}
+      {/* <HW2 /> */}
+      {/* <HW3 /> */}
+      <HW4 />
     </div>
   );
 }
